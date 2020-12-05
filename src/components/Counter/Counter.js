76c93f9003f00d1cdf124a4bb51f5639d9f35abf
Vadym Usov <onlinemovement@gmail.com>
2020-12-05 19:34:32 +0200

    added onClick for the buttons

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,13 +2,27 @@ import React from 'react';
 import './Counter.css';
 
 class Counter extends React.Component {
+  handleIncrement = e => {
+    console.log('Нажали кнопку +');
+    console.log(e.target);
+  };
+
+  handleDecrement = e => {
+    console.log('Нажали кнопку -');
+    console.log(e.target);
+  };
+
   render() {
     return (
       <div className="Counter">
         <span className="Counter__value">0</span>
         <div className="Counter__controls">
-          <button type="button">+1</button>
-          <button type="button">-1</button>
+          <button type="button" onClick={this.handleIncrement}>
+            +1
+          </button>
+          <button type="button" onClick={this.handleDecrement}>
+            -1
+          </button>
         </div>
       </div>
     );
