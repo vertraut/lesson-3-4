40ae9fc6d7d46dbe99b27e8da312f689099d6ec7
Vadym Usov <onlinemovement@gmail.com>
2020-12-06 16:03:52 +0200

    adds component ColorPicker

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 // import Counter from './components/Counter';
-import Counter from './components/Dropdown';
+// import Counter from './components/Dropdown';
 import './App.css';
-import Dropdown from './components/Dropdown';
+// import Dropdown from './components/Dropdown';
+import ColorPicker from './components/ColorPicker';
+
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
 
 function App() {
   return (
     <>
       <h1>Состояние компонента</h1>
       {/* <Counter /> */}
-      <Dropdown />
+      {/* <Dropdown /> */}
+      <ColorPicker options={colorPickerOptions} />
     </>
   );
 }
